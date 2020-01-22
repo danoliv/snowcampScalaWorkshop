60d64f29ae8f23a98fcc0c56344e4af5a27a95dd
Andrea Baita <andrea.baita@kelkoo.com>
2020-01-22 12:44:31 +0100

    implement step 4 and 5

--- src/main/scala/tanks/MyTank.scala
+++ src/main/scala/tanks/MyTank.scala
@@ -1,6 +1,7 @@
 package tanks
 
 import game.Key
+import Key._
 import graphic.Draw._
 import graphic.{GraphicContext, Point}
 
@@ -19,14 +20,35 @@ case class MyTank(
   }
 
   // TODO 4) compute the new position by combining up and down keys
-  def computePosition(pressedKeys: Set[Int]): Point = position
+  def computePosition(pressedKeys: Set[Int], bounds: Point): Point = {
+
+    pressedKeys.foldRight(position){ case (key: Int, currentPosition: Point) =>
+
+      key match {
+        case `up` => move(currentPosition, maxSpeed, bounds)
+        case `down` => move(currentPosition, -maxSpeed, bounds)
+        case _ => currentPosition
+      }
+    }
+
+  }
 
   // TODO 5) compute the new angle by combining left and right keys
-  def computeAngle(pressedKeys: Set[Int]): Double = angle
+  def computeAngle(pressedKeys: Set[Int]): Double = {
+    pressedKeys.foldRight(angle){ case (key: Int, currentAngle: Double) =>
+
+      key match {
+        case `left` => rotation(currentAngle, -stepRotation)
+        case `right` => rotation(currentAngle, +stepRotation)
+        case _ => currentAngle
+      }
+    }
+
+  }
 
   def update(pressedKeys: Set[Int], releasedKeys: Set[Int], bounds: Point): (MyTank, Set[Bullet]) = {
     import Key._
-    val newPosition = computePosition(pressedKeys)
+    val newPosition = computePosition(pressedKeys, bounds)
 
     val newAngle = computeAngle(pressedKeys)
 
